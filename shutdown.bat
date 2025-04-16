c960d9e46b14803dd271e90271957bb5e23e7c21
SaybroWT21 <130052435+SaybroWT21@users.noreply.github.com>
2025-04-16 12:27:23 +0200

    Add files via upload

--- shutdown.bat
+++ shutdown.bat
@@ -1,10 +1,10 @@
 @echo off
 
 echo clearing DNS log
-timeout /t 1
+timeout /t 1 /nobreak
 ipconfig /flushdns
 echo Onto the events log
-timeout /t 1
+timeout /t 1 /nobreak
 
 FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
 IF (%adminTest%)==(Access) goto noAdmin
@@ -26,7 +26,7 @@ pause >nul
 
 :shutdown
 echo Shutting down now
-timeout /t 1
+timeout /t 1 /nobreak
 shutdown /f /p
 
 Exit
